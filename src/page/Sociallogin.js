3028b2b4df90e9fa345731c07310b277b1238472
ashwinlaly <ashwinlaly@gmail.com>
2020-05-12 12:25:45 +0530

    Social Login

---
+++ src/page/Sociallogin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sociallogin = () => {
+    return (
+        <div>
+            <button onClick={() => {
+                
+            }}>Click</button>
+        </div>
+    )
+}
